7409b305ce59eea68f5dee9b2436000467b5107b
Ismail el korchi <ismail.elkorchi@gmail.com>
2017-10-02 13:12:45 +0100

    Establish the Basic Overall Layout

--- 404.php
+++ 404.php
@@ -10,52 +10,54 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="wrap">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'tasty' ); ?></h1>
-				</header><!-- .page-header -->
+				<section class="error-404 not-found">
+					<header class="page-header">
+						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'tasty' ); ?></h1>
+					</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'tasty' ); ?></p>
+					<div class="page-content">
+						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'tasty' ); ?></p>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'tasty' ); ?></h2>
-						<ul>
 						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
+							get_search_form();
+
+							the_widget( 'WP_Widget_Recent_Posts' );
 						?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
+						<div class="widget widget_categories">
+							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'tasty' ); ?></h2>
+							<ul>
+							<?php
+								wp_list_categories( array(
+									'orderby'    => 'count',
+									'order'      => 'DESC',
+									'show_count' => 1,
+									'title_li'   => '',
+									'number'     => 10,
+								) );
+							?>
+							</ul>
+						</div><!-- .widget -->
 
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'tasty' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						<?php
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+							/* translators: %1$s: smiley */
+							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'tasty' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+							the_widget( 'WP_Widget_Tag_Cloud' );
+						?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					</div><!-- .page-content -->
+				</section><!-- .error-404 -->
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div>
 
 <?php
 get_footer();
--- footer.php
+++ footer.php
@@ -14,19 +14,21 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tasty' ) ); ?>">
+		<div class="wrap">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tasty' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'tasty' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> | </span>
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'tasty' ), 'WordPress' );
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'tasty' ), 'tasty', '<a href="http://www.ismailelkorchi.com">Ismail El Korchi</a>' );
 				?>
-			</a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tasty' ), 'tasty', '<a href="http://www.ismailelkorchi.com">Ismail El Korchi</a>' );
-			?>
-		</div><!-- .site-info -->
+			</div><!-- .site-info -->
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
